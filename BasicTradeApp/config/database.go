72694efe36508c07d8b68cc3aaa6df0140776cd1
Farida Arum Parwati <faridaarum1727@gmail.com>
2024-06-30 20:09:58 +0700

    update environment variables for railway

--- BasicTradeApp/config/database.go
+++ BasicTradeApp/config/database.go
@@ -17,7 +17,7 @@ func ConnectDB() {
 	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found")
 	}
 
 	// Get database credentials from environment variables
