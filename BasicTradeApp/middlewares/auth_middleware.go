3690b27821c48dac41e5969a19276828f80183e5
Farida Arum Parwati <faridaarum1727@gmail.com>
2024-06-30 22:30:47 +0700

    change middleware

--- BasicTradeApp/middlewares/auth_middleware.go
+++ BasicTradeApp/middlewares/auth_middleware.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"BasicTradeApp/services"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,13 +17,23 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		_, err := services.ValidateToken(tokenString)
+		// Extract token
+		parts := strings.Split(tokenString, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header format must be Bearer {token}"})
+			c.Abort()
+			return
+		}
+
+		tokenString = parts[1]
+		claims, err := services.ValidateToken(tokenString)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
 			c.Abort()
 			return
 		}
 
+		c.Set("username", claims.Username)
 		c.Next()
 	}
 }
